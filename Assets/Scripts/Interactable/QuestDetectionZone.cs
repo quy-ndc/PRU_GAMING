4142270caee1d9a0c1a34d2795c86a914dd721e0
Quý <toilachungquy6789@gmail.com>
2024-05-22 20:53:47 +0700

    add level 7

--- Assets/Scripts/Interactable/QuestDetectionZone.cs
+++ Assets/Scripts/Interactable/QuestDetectionZone.cs
@@ -6,23 +6,10 @@ public class QuestDetectionZone : MonoBehaviour
 {
     public List<Collider2D> detectedCollider = new List<Collider2D>();
     public int colliderAmount;
-    Collider2D col;
-    
-    private void Awake()
-    {
-        col = GetComponent<Collider2D>();
-    }
 
     private void Update()
     {
         colliderAmount = detectedCollider.Count;
-        foreach (Collider2D collider in detectedCollider)
-        {
-            if (!collider.GetComponent<TreeController>().IsGrown)
-            {
-                collider.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
-            }
-        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
